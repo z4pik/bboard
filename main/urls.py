6d9094eff898612a9f8ac3032703ac30e6cf0ed9
z4pik <segafurov@gmail.com>
2022-11-24 13:49:28 +0300

    Сделал регистрацию

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import index, other_page
+from .views import index, other_page, BbLoginView
 
 app_name = 'main'
 
 urlpatterns = [
+
+    # Указан именно данный путь (accounts/login/), потому что по умолчанию django выполняет перенаправление
+    # именно на этот адрес, когда пользователь хочет получить доступ к закрытой странице
+    path('accounts/login/', BbLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index')
 ]
--- main/views.py
+++ main/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
+from django.contrib.auth.views import LoginView
 
 
 def index(request):
+    # Главная
     return render(request, 'main/index.html')
 
 
@@ -17,3 +19,9 @@ def other_page(request, page):
         raise Http404
     # В любом случае возвращаем ответ
     return HttpResponse(template.render(request=request))
+
+
+class BbLoginView(LoginView):
+    # Логин
+    template_name = 'main/login.html'
+
