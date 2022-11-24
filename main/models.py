a6540f3294594c254a8da2d771ecb1a605a24871
z4pik <segafurov@gmail.com>
2022-11-24 13:28:25 +0300

    Расписал модель пользователя

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class AdvUser(AbstractUser):
+    is_activated = models.BooleanField(default=True, db_index=True,
+                                       verbose_name="Прошел активацию?")
+    is_messages = models.BooleanField(default=True,
+                                      verbose_name="Слать-ли оповещения о новых сообщениях?")
+
+    class Meta(AbstractUser.Meta):
+        pass
