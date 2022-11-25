b9acf8eed1c9cb49bdf80b8cdc9a72c9c43dbbaf
z4pik <segafurov@gmail.com>
2022-11-25 05:26:19 +0300

    Выб-страница выхода из аккаунта

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import index, other_page, BbLoginView, profile
+from .views import index, other_page, BbLoginView, profile, BbLogoutView
 
 app_name = 'main'
 
@@ -8,10 +8,11 @@ urlpatterns = [
 
     # Указан именно данный путь (accounts/profile), потому что по умолчанию django выполняет перенаправление
     # именно на этот адрес, после успешного входа
-    path('accounts/profile', profile, name='profile'),
+    path('accounts/profile/', profile, name='profile'),
     # Указан именно данный путь (accounts/login/), потому что по умолчанию django выполняет перенаправление
     # именно на этот адрес, когда пользователь хочет получить доступ к закрытой странице
     path('accounts/login/', BbLoginView.as_view(), name='login'),
+    path('accounts/logout/', BbLogoutView.as_view(), name='logout'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index')
 ]
--- main/views.py
+++ main/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 
 
@@ -32,3 +33,9 @@ def profile(request):
     # Профиль пользователя
     return render(request, 'main/profile.html')
 
+
+class BbLogoutView(LoginRequiredMixin, LogoutView):
+    """Страница выхода должна быть доступна только зарегистрированным пользователям, которые выполнили вход.
+    Поэтому я и добавил LoginRequiredMixin """
+    template_name = 'main/logout.html'
+    
